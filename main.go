6eda02404539276ae0fe63be5d3b6c34b263e5f9
Zach Calvert <ZacharyCalvert+ghub@gmail.com>
2021-02-20 20:46:33 -0600

    Added yml parsing capability

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var loadDatabase string
+
+	flag.StringVar(&loadDatabase, "db", "./pic-man.db", "The full path for pic-man database")
+
+	if _, err := os.Stat(loadDatabase); err == nil {
+		fmt.Printf("Will load database from %v\n", loadDatabase)
+	} else {
+		panic(fmt.Errorf("missing %v - pic manager database", loadDatabase))
+	}
+
+	data, err := ioutil.ReadFile(loadDatabase)
+	if err != nil {
+		panic(fmt.Errorf("could not read %v: %w", loadDatabase, err))
+	}
+
+	allImages := make(map[string]ImageMeta)
+
+	err = yaml.Unmarshal([]byte(data), &allImages)
+	if err != nil {
+		panic(fmt.Errorf("failed to load %v: %w", loadDatabase, err))
+	}
+	fmt.Printf("Loaded %v meta records", len(allImages))
+}
+
+// ImageMeta is a single entry in our pic-man.db
+type ImageMeta struct {
+	Sha        string   `yaml:"sha256"`
+	Extensions []string `yaml:"extensions"`
+	Paths      []string `yaml:"paths"`
+	Date       uint64   `yaml:"earliestDate"`
+	Review     bool     `yaml:"reviewDone"`
+	Ignore     bool     `yaml:"ignore"`
+}
+
+func (meta ImageMeta) String() string {
+	return fmt.Sprintf("Meta{sha: %v, pathCount: %v, date: %v, reviewDone: %v, Ignore: %v}")
 }
